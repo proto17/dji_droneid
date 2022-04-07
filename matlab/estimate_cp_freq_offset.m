28e5baab9760be8c7600a8e2d1f520d2fadae1be
David Protzman <NotTelling@NoThanks.Poop>
2022-04-07 15:59:30 -0400

    Now plotting the cyclic prefix estimations

--- matlab/estimate_cp_freq_offset.m
+++ matlab/estimate_cp_freq_offset.m
@@ -1,5 +1,5 @@
 function [freq_offset_est] = estimate_cp_freq_offset(samples, fft_size, short_cp_len, long_cp_len)
-    
+    figure(44);
     scores = zeros(9, 1);
     skip = 8;
     sample_offset = 1;
@@ -14,6 +14,7 @@ function [freq_offset_est] = estimate_cp_freq_offset(samples, fft_size, short_cp
         cyclic_prefix = samples(sample_offset+skip:sample_offset + cp_len - 1 - skip);
         end_of_symbol = samples(sample_offset+fft_size+skip:sample_offset+fft_size+cp_len - 1 - skip);
         
+        subplot(3, 3, idx);
         plot(abs(cyclic_prefix).^2);
         hold on;
         plot(abs(end_of_symbol).^2);
